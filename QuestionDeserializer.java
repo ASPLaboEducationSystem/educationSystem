bbf1d3acf7bccb5b131f17f5b839801b654195ad
LAPTOP-L9JA9ATK\580da <22aj050@ms.dendai.ac.jp>
2025-02-02 05:24:17 +0900

    jsonファイルとtextファイルのディレクトリを作成

--- QuestionDeserializer.java
+++ QuestionDeserializer.java
@@ -7,7 +7,7 @@ public class QuestionDeserializer{
     // categoryを追加して、jsonファイルを1つにする。
     private static final String[] FILENAME = {"1.json", "2.json", "3.json", "4.json", "5.json", "6.json", "7.json", "8.json", "9.json", "10.json"};
     public static QuestionList getQuestionList(int selectedUnitNumber){
-        String all = JsonUtils.readAll(Paths.get(FILENAME[selectedUnitNumber - 1]));
+        String all = JsonUtils.readAll(Paths.get("./json/" + FILENAME[selectedUnitNumber - 1]));
         Jsonb jsonb = JsonbBuilder.create();
         QuestionList questionList = jsonb.fromJson(all, QuestionList.class);
         return questionList;
--- ShowBasicGrammer.java
+++ ShowBasicGrammer.java
@@ -6,8 +6,7 @@ public class ShowBasicGrammer implements Usecase{
     private final String[] FILENAME = {"1.txt", "2.txt", "3.txt", "4.txt", "5.txt", "6.txt", "7.txt", "8.txt", "9.txt", "10.txt"};
     @Override
     public void start(Educatee educatee, int selectedUnitNumber){
-        String data = PathUtils.readAll(Paths.get(this.FILENAME[selectedUnitNumber - 1]), Charset.forName("UTF-8"));
+        String data = PathUtils.readAll(Paths.get("./text/" + this.FILENAME[selectedUnitNumber - 1]), Charset.forName("UTF-8"));
         System.out.println(data);
     }
 }
-
